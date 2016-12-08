8f6bf7243556dbe7990168e244b62576b155a279
baursak <baursak@gmail.com>
2016-12-08 12:56:36 +0300

    lesson 04: code in JS way, long jQuery way and short jQuery way (all in site.js)

---
+++ js/site.js
@@ -0,0 +1,12 @@
+// ... JavaScript way ...
+// window.alert('Here is some text');
+
+// ... long way of writing jQuery code ...
+// $(document).ready(function(){
+//
+// });
+
+// ... short way of writing jQuery code ...
+$(function(){
+    
+});

b95824f9ba0dd48de1225baa55fc15e6fb521a88
baursak <baursak@gmail.com>
2016-12-08 13:58:07 +0300

    lesson 06: slideUp, slideDown, slideToggle

--- js/site.js
+++ js/site.js
@@ -1,3 +1,11 @@
-$(function(){
-    $("p").not("#second").css("color", "#f00");
+$(function () {
+    $(".close-btn").click(function () {
+        $(".slider").slideUp();
+    });
+    $(".open-btn").click(function () {
+        $(".slider").slideDown();
+    });
+    $(".toggle-btn").click(function () {
+        $(".slider").slideToggle();
+    });
 });

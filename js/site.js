f653139857180f5c3698fd1b4c1e18e3225be25b
baursak <baursak@gmail.com>
2016-12-08 14:26:02 +0300

    lesson 07: plugged Open Sans font via site.css

--- js/site.js
+++ js/site.js
@@ -1,11 +1,3 @@
 $(function () {
-    $(".close-btn").click(function () {
-        $(".slider").slideUp();
-    });
-    $(".open-btn").click(function () {
-        $(".slider").slideDown();
-    });
-    $(".toggle-btn").click(function () {
-        $(".slider").slideToggle();
-    });
+
 });
